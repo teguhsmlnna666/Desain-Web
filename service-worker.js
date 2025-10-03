ebd53f5dcce48ff382b9ab024a7e0a38f5b659e8
teguhsmlnna666 <teguhesamaulanna566@gmail.com>
2025-10-03 20:42:34 +0700

    first commit

---
+++ service-worker.js
@@ -0,0 +1,38 @@
+const CACHE_NAME = "teguh-pwa-v1";
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/about.html",
+  "/contact.html",
+  "/offline.html",
+  "/style.css",
+  "/icons/icon-192.png",
+  "/icons/icon-512.png"
+];
+
+// Install
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+// Activate
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) =>
+      Promise.all(
+        cacheNames.filter((name) => name !== CACHE_NAME).map((name) => caches.delete(name))
+      )
+    )
+  );
+});
+
+// Fetch
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    fetch(event.request).catch(() =>
+      caches.match(event.request).then((resp) => resp || caches.match("/offline.html"))
+    )
+  );
+});
